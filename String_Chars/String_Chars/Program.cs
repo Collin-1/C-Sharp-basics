d167cd12330ae24f7d9fd8dc52a09675309a1c4c
Collin Makwala <collinmakwala@gmail.com>
2025-02-07 21:17:24 +0200

    Hello World!

---
+++ String_Chars/String_Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            char letter = 'a';
+
+            Console.Write("My name is ");
+            Console.WriteLine(name);
+            Console.WriteLine();
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+
+       
+        }
+    }
+}
