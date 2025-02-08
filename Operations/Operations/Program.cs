8bb67eeefe7bfe1382e5690ee60178a9052c69a0
Collin Makwala <collinmakwala@gmail.com>
2025-02-08 16:29:54 +0200

    second commit

---
+++ Operations/Operations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = 23;
+            //age++; //can only increment by 1
+            //age = age + 1;
+            age /= 10;
+            Console.WriteLine(age);
+
+            string name = "Aba";
+            name += " is programming!";
+
+            Console.WriteLine(name);
+                
+            int i = 0;
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+    }
+}
