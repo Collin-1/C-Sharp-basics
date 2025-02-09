4fe3e0d21375699fd4115ac4cc85812d6d4e6349
Collin Makwala <collinmakwala@gmail.com>
2025-02-09 20:56:09 +0200

    tryParse

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string numInput = Console.ReadLine();
+            int num = Convert.ToInt32(numInput);
+
+            Console.WriteLine(num);
+
+
+            Console.ReadLine();
+        }
+    }
+}
