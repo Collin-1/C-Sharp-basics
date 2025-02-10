f1d273f033cbb4e5ab4ca2b8e2280655d7bff3a3
Collin Makwala <collinmakwala@gmail.com>
2025-02-10 20:16:39 +0200

    sixth

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> names = new Dictionary<int, string>()
+            {
+                { 1, "Collin"},
+                { 2, "Aba" },
+                { 3, "Mike" }
+            };
+            /*
+            names.Add(1, "Collin");
+            names.Add(2, "Aba");
+            names.Add(3, "Mike");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names.Values.ElementAt(i));
+            }*/
+
+            for (int i = 1; i < names.Count; i++)
+            {
+                Console.WriteLine(names.ElementAt(i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ List/List/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNums = new int[4]
+            {
+                2, 4, 1, 5
+            };
+
+            List<int> listNums = new List<int>()
+            {
+                2, 4, 1, 5, 2
+            };
+
+            for ( int i = 0; i < listNums.Count; i++)
+            {
+                Console.WriteLine(listNums[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
