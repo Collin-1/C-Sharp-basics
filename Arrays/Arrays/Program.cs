d29f608330ffaa32bace18a0803ca2989eb38912
Collin Makwala <collin.makwala@ogilvy.co.za>
2025-02-10 15:46:28 +0200

    fourth Commit

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int num3 = 25;
+
+            int[] numbers = new int[3];
+
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (char i in "hello")
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]
+            {
+                6,2, 7, 4, 5, 9, 3, 8, 1
+            };
+
+
+
+            foreach (int i in nums)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Array.Sort(nums);
+
+            foreach (int i in nums)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Array.Reverse(nums);
+
+            foreach (int i in nums)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+
+            Array.Clear(nums, 5, 4);
+            foreach (int i in nums)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
